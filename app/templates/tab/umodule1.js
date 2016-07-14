1f4bef835821481d7d92bafe3f1ec00f607575fd
litor <547794529@qq.com>
2016-07-14 15:57:39 +0800

    commit

--- app/templates/tab/umodule1.js
+++ app/templates/tab/umodule1.js
@@ -1,4 +1,4 @@
-define(function(require, exports, module) {
+define(function(require) {
 
   var utils = require('utils');
   var bs = require('./<%=moduleName %>BS');
--- app/templates/tab/umodule1BS.js
+++ app/templates/tab/umodule1BS.js
@@ -1,4 +1,4 @@
-define(function(require, exports, module) {
+define(function(require) {
 
   var utils = require('utils');
   var mock = require('./mock');
